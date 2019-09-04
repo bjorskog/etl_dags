edc18e0a55c02a7c114f573fec4d25919232972e
Bjørn Waage Skogtrø <bjorn.skogtro@gmail.com>
2019-09-04 21:34:54 +0200

    Inital commit

---
+++ dags/hello_world.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+from homemade import hello_function
+
+
+dag = DAG(
+    'hello_world', 
+    description='Simple tutorial DAG',
+    schedule_interval='0 12 * * *',
+    start_date=datetime(2017, 3, 20), 
+    catchup=False)
+
+dummy_operator = DummyOperator(
+    task_id='dummy_task', retries=3, dag=dag
+    )
+
+hello_operator = PythonOperator(
+    task_id='hello_task', 
+    python_callable=hello_function.print_hello, 
+    dag=dag
+    )
+
+dummy_operator.set_downstream(hello_operator)
+
+# dummy_operator >> hello_operator
---
+++ dags/homemade/hello_function.py
@@ -0,0 +1,6 @@
+
+
+
+def print_hello():
+    """ returns Hello """
+    return 'Hello world!'
