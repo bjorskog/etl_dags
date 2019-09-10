8cc49508075a113486136c66d8603fb2d2363ca4
Bjørn Waage Skogtrø <bjorn.skogtro@gmail.com>
2019-09-10 19:48:04 +0200

    Added papermill example

---
+++ dags/papermill_example.py
@@ -0,0 +1,56 @@
+
+"""
+Using Papermill to run a Jupyter-notebook
+using Airflow
+"""
+
+import os
+import papermill as pm
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+def execute_python_notebook_task(**context):
+    notebook_path = context['notebook_path']
+    out_path = context['out_path']
+    out_dir = os.path.dirname(out_path)
+    statement_parameters = context['statement_parameters'] if 'statement_parameters' in context else None
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    if callable(statement_parameters):
+        statement_parameters = statement_parameters(context)
+
+    pm.execute_notebook(
+        notebook_path,
+        out_path,
+        parameters=statement_parameters
+    )
+
+seven_days_ago = datetime.combine(
+    datetime.today() - timedelta(7),
+    datetime.min.time()
+)
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': seven_days_ago,
+    'provide_context': True,
+}
+
+dag_name = 'runnin_notebooks_yo'
+schedule_interval = '@monthly'
+
+with DAG(dag_name, default_args=default_args, schedule_interval=schedule_interval) as dag:
+    run_some_notebook_task = PythonOperator(
+        task_id='run_some_notebook_task',
+        python_callable=execute_python_notebook_task,
+        op_kwargs={
+            'notebook_path': 'gs://insr-notebooks-storage/example.ipynb',
+            'out_path': 'gs://insr-notebooks-storage/example.ipynb',
+            'statement_parameters': {
+                'a': 120
+            }
+        }
+    )
